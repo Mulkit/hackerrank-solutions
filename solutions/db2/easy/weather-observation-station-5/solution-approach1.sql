14caec2cb7c42716ba3d01115d5637465cc3c723
Mulkit Sain <88576581+Mulkit@users.noreply.github.com>
2026-08-30 22:45:48 +0530

    ✅ Weather Observation Station 5 [db2] — approach1

---
+++ solutions/db2/easy/weather-observation-station-5/solution-approach1.sql
@@ -0,0 +1,28 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-5/problem?isFullScreen=true
+-- Problem     Weather Observation Station 5
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    db2
+-- Status      Accepted
+-- Submitted   2026-08-30, 10:45 p.m.
+-- ──────────────────────────────────────────────────
+
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+
+SELECT city, length(city)
+from station
+order by length(city) asc, city ASC
+limit 1;
+
+SELECT city, length(city)
+from station
+order by length(city) desc, city ASC
+limit 1;
