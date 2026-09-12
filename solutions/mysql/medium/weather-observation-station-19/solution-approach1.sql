d376100a05167aa68ed1fb058e068e507d7cc80a
Mulkit Sain <88576581+Mulkit@users.noreply.github.com>
2026-09-12 21:29:19 +0530

    ✅ Weather Observation Station 19 [mysql] — approach1

---
+++ solutions/mysql/medium/weather-observation-station-19/solution-approach1.sql
@@ -0,0 +1,29 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-19/problem?isFullScreen=true
+-- Problem     Weather Observation Station 19
+-- Difficulty  Medium
+-- Subdomain   Aggregation
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-12, 09:29 p.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+ You need the distance between:
+
+Point 1: (MIN(LAT_N), MIN(LONG_W))
+Point 2: (MAX(LAT_N), MAX(LONG_W))
+
+The Euclidean distance formula is:
+
+$$ \sqrt{(x_2-x_1)^2+(y_2-y_1)^2} $$
+
+*/
+
+SELECT ROUND(SQRT(POWER(MAX(LAT_N) - MIN(LAT_N),2) + POWER(MAX(LONG_W) - MIN(LONG_W),2)) , 4 ) 
+FROM STATION;
+
+
+
