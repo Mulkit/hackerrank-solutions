d096df6bc4a56a007c355418a91aadd301384490
Mulkit Sain <88576581+Mulkit@users.noreply.github.com>
2026-09-12 15:09:25 +0530

    ✅ Average Population [mysql] — approach1

---
+++ solutions/mysql/easy/average-population/solution-approach1.sql
@@ -0,0 +1,14 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/average-population/problem?isFullScreen=true
+-- Problem     Average Population
+-- Difficulty  Easy
+-- Subdomain   Aggregation
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-12, 03:09 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT ROUND(AVG(POPULATION)) FROM CITY;
+
+
