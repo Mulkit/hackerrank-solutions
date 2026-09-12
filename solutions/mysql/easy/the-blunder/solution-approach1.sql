9c473d819ef365b9f5d8972d6d6f10e1ba417bfc
Mulkit Sain <88576581+Mulkit@users.noreply.github.com>
2026-09-12 15:27:11 +0530

    ✅ The Blunder [mysql] — approach1

---
+++ solutions/mysql/easy/the-blunder/solution-approach1.sql
@@ -0,0 +1,17 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/the-blunder/problem?isFullScreen=true
+-- Problem     The Blunder
+-- Difficulty  Easy
+-- Subdomain   Aggregation
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-12, 03:27 p.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/
+
+
+SELECT CEIL(AVG(SALARY) - AVG(REPLACE(SALARY, '0',''))) FROM EMPLOYEES;
