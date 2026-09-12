0c3b2439ee6c9d495108f1f298aaaf53986b82ae
Mulkit Sain <88576581+Mulkit@users.noreply.github.com>
2026-09-12 17:40:56 +0530

    ✅ Weather Observation Station 17 [mysql] — approach1

---
+++ solutions/mysql/easy/weather-observation-station-17/solution-approach1.sql
@@ -0,0 +1,22 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-17/problem?isFullScreen=true
+-- Problem     Weather Observation Station 17
+-- Difficulty  Easy
+-- Subdomain   Aggregation
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-12, 05:40 p.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/
+
+SELECT ROUND(LONG_W,4) FROM STATION
+WHERE LAT_N = (
+    SELECT MIN(LAT_N) FROM STATION
+    WHERE LAT_N > 38.7780);
+
+
+
