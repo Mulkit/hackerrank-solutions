2fb9c59de3141b98d99482c7cf43e24f193d4d93
Mulkit Sain <88576581+Mulkit@users.noreply.github.com>
2026-09-12 13:19:08 +0530

    ✅ Employee Names [mysql] — approach1

---
+++ solutions/mysql/easy/name-of-employees/solution-approach1.sql
@@ -0,0 +1,16 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true
+-- Problem     Employee Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-12, 01:19 p.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/
+
+SELECT NAME FROM EMPLOYEE ORDER BY NAME ASC;
